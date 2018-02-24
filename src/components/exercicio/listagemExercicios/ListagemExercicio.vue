ab2c692569f000e9da4050b8a01854f377dbd076
JuanesAdriano <juaneshtk50@gmail.com>
2018-02-24 13:34:37 -0300

    Fazendo listinha para renderizar os objetos retornados da coleção de exercícios.

--- src/components/exercicio/listagemExercicios/ListagemExercicio.vue
+++ src/components/exercicio/listagemExercicios/ListagemExercicio.vue
@@ -2,7 +2,18 @@
   <v-container>
     <v-flex xs11 offset-xs1><h3>Listagem de Exercícios</h3></v-flex>
     <v-layout row>
-      <v-flex sm11 offset-sm1>Response:{{exercicios}}</v-flex>
+      <v-flex sm4 offset-sm1>
+        <v-list>
+          <v-list-tile v-for="ex in exercicios" :key="ex.codigo" ripple>
+
+                <v-list-tile-title v-html="ex.nome" tag="<h2>"></v-list-tile-title>
+            <v-list-tile-action>
+              <v-button class="info" >VER</v-button>
+            </v-list-tile-action>
+
+          </v-list-tile>
+        </v-list>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
@@ -11,19 +22,21 @@
   import db from '../../firebaseFiles/firebaseInit'
 
   export default {
-    data(){
-      return{
-        exercicios:[]
+    data() {
+      return {
+        exercicios: []
       }
     },
-    created(){
+    created() {
       db.collection('exercicios').get()
-        .then(querySnapshot =>{
-          querySnapshot.forEach(doc=>{
-            console.log(doc.data());
-            const data={
-
+        .then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            const data = {
+              'codigo': doc.id,
+              'nome': doc.data().nome,
+              'quadrante': doc.data().quadrante
             }
+            this.exercicios.push(data)
           })
         })
     }
